e094adc9efba9897a8fa6aec36e07ef629f09d58
ogav <olenagavrishchuk@gmail.com>
2020-03-15 21:38:29 -0700

    Added read method

--- ThreadOS/FileSystem.java
+++ ThreadOS/FileSystem.java
@@ -22,7 +22,40 @@ public class FileSystem {
     }
 
     void read(FileTableEntry entry, byte[] data) {
+        if (entry != null && !entry.mode.equals("w") && !entry.mode.equals("a")) {
+            synchronized (entry) {
+                int fsize = fsize(entry);
+                int read = 0;
+                int bufleft = data.length;
 
+                while (bufleft > 0 && entry.seekPtr < fsize) {
+                    int id = entry.inode.findTargetBlock(entry.seekPtr);
+                    if (id >= 0) {
+                        byte[] block = new byte[Disk.blockSize];
+                        SysLib.rawread(id, block);
+
+                        int init = entry.seekPtr % Disk.blockSize;
+
+                        int blcktoRead = Disk.blockSize - init;
+                        int dataLeft = fsize - entry.seekPtr;
+                        int toRead;
+
+                        if (bufleft < blcktoRead && bufleft < dataLeft)
+                            toRead = bufleft;
+                        else if (dataLeft < blcktoRead && dataLeft < bufleft)
+                            toRead = dataLeft;
+                        else
+                            toRead = blcktoRead;
+
+                        System.arraycopy(block, init, data, read, toRead);
+
+                        entry.seekPtr += toRead;
+                        read += toRead;
+                        bufleft -= toRead;
+                    }
+                }
+            }
+        }
     }
 
     FileTableEntry open(String name, String mode) {
